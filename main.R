fe40989ebc0f4f460054902bc1553007c883136f
Pedro Sartori <pedrosartdr@gmail.com>
2023-09-11 23:37:34 -0300

    radar chart

--- main.R
+++ main.R
@@ -3,6 +3,8 @@ setwd('C:/Users/Lenovo/Desktop/automation')
 library(leaflet)
 library(dplyr)
 library(ggplot2)
+library(fmsb)
+
 source('utils/convertions.R')
 
 
@@ -10,6 +12,8 @@ source('utils/convertions.R')
 df <- data.frame(
   x = c(1, 2, 3, 4),
   y = c(3, 7, 5, 1),
+  z = c(2, 5, 4, 2),
+  h = c(1, 2, 3, 1),
   lat = c(-21.15712, -21.19058, -21.21523, -21.17922),
   lon = c(-47.79177, -47.83451, -47.79657, -47.71812)
 )
@@ -41,6 +45,25 @@ map = leaflet(df) %>% addTiles() %>%
   addMarkers(~lon, ~lat, icon = icons)
 map
 
+# radar chart
+#df_radar = df[c(1:4)] %>% slice(1)
+df_radar = df[c(1:4)]
+df_radar = rbind(rep(7, 4), rep(0, 4), df_radar)
+
+colors_border=c( rgb(0.2,0.5,0.5,0.6), rgb(0.8,0.2,0.5,0.6) , rgb(0.7,0.5,0.1,0.6), rgb(0.3,0.7,0.1,0.6) )
+colors_in=c( rgb(0.2,0.5,0.5,0.2), rgb(0.8,0.2,0.5,0.2) , rgb(0.7,0.5,0.1,0.2), rgb(0.3,0.7,0.1,0.2) )
+
+radar = function() {
+  radarchart(df_radar, axistype=1, 
+           #custom polygon
+           pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1,
+           #custom the grid
+           cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(0, 7, 1.75), cglwd=0.8,
+           #custom labels
+           vlcex=2 )
+}
+radar()
+
 # saving charts
 width = 900
 height = 700
@@ -51,6 +74,11 @@ saveGgplotAsImage(chart3, 'charts/chart3.jpeg', widthPx = width, heightPx = heig
 # saving maps
 saveLeafletAsImage(map, 'charts/map.jpeg', outputHtmlPath = 'maps/map.html',
                    widthPx = 800, heightPx = 500)
+
+# saving radar
+jpeg('charts/radar.jpeg', width = 600, height = 600)
+radar()
+dev.off()
 
 # saving pages
 width = 1280
