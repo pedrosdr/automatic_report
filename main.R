fead1b0d06ade6e0f961f07728526eb775a6c87f
Pedro Sartori <pedrosartdr@gmail.com>
2023-09-21 17:58:09 -0300

    relative label positions

--- main.R
+++ main.R
@@ -1,21 +1,26 @@
 setwd('C:/Users/Lenovo/Desktop/automation')
 
+#install.packages("remotes")
+#remotes::install_github("ricardo-bion/ggradar")
+
 library(leaflet)
 library(dplyr)
 library(ggplot2)
 library(fmsb)
+# library(ggradar)
+library(ggiraphExtra)
 
 source('utils/convertions.R')
 
-
 # creating dataframe
 df <- data.frame(
-  x = c(1, 2, 3, 4),
-  y = c(3, 7, 5, 1),
-  z = c(2, 5, 4, 2),
-  h = c(1, 2, 3, 1),
-  lat = c(-21.15712, -21.19058, -21.21523, -21.17922),
-  lon = c(-47.79177, -47.83451, -47.79657, -47.71812)
+  names = c('a', 'b', 'c', 'd', 'e', 'f', 'g'),
+  x = c(1, 2, 3, 4, 5, 6, 7),
+  y = c(3, 7, 5, 1, 3, 3, 2),
+  z = c(2, 5, 4, 2, 9, 9, 2),
+  h = c(1, 2, 3, 1, 2, 1, 9),
+  lat = c(-21.15712, -21.19058, -21.15674, -21.17922, -21.16182, -21.16770, -21.18363),
+  lon = c(-47.79177, -47.83451, -47.79678, -47.71812, -47.79230, -47.80270, -47.71732)
 )
 
 # creating charts
@@ -33,21 +38,139 @@ chart3 = df %>% ggplot(aes(x = x, y = y)) +
 chart3
 
 # creating map
-icons = icons(
-  iconUrl = ifelse(df$x == 1, 'markers/red_marker.png',
-            ifelse(df$x == 2, 'markers/orange_marker.png',
-            ifelse(df$x == 3, 'markers/yellow_marker.png', 'markers/green_marker.png'))),
-  iconWidth = 38, iconHeight = 38,
-  iconAnchorX = 19, iconAnchorY = 38,
-)
+# max longitude - min longitude
+
+# df -> dataframe(lat, lon)
+getSpan = function(d) {
+  return(abs(max(d[,2]) - min(d[,2])))
+}
+
+# getting positions of the marker labels (center, left, right)
+# df -> dataframe(lat, lon)
+getPositions = function(d) 
+{
+  positions = c()
+  
+  for(i in 1:nrow(d)) 
+  {
+    lat_i = d[i, 1]
+    lon_i = d[i, 2]
+    
+    distances_i = data.frame()
+    
+    for(j in 1:nrow(d)) 
+    {
+      if(i == j) next
+      lat_j = d[j, 1]
+      lon_j = d[j, 2]
+      
+      if(abs(lat_i - lat_j) < 0.1 * getSpan(d) && abs(lon_i - lon_j < 0.1 * getSpan(d)))
+        distances_i = rbind(distances_i, c(lat_i - lat_j, lon_i - lon_j))
+    }
+    
+    print(distances_i)
+    
+    
+    if(nrow(distances_i) > 0 && !is.null(distances_i))
+    {
+      colnames(distances_i) = c('lat_dist', 'lon_dist')
+      distances = summarise(distances_i, lat_dist = mean(lat_dist), lon_dist = mean(lon_dist))
+      
+      if(distances$lon_dist <= 0 && distances$lat >= 0)
+      {
+        positions = c(positions, "left")
+      } else if(distances$lon_dist > 0 && distances$lat >= 0)
+      {
+        positions = c(positions, "right")
+      } else
+      {
+        positions = c(positions, "center")
+      }
+    }
+    else {
+      positions = c(positions, "center")
+    }
+  }
+    
+  return(positions)
+}
+
+df$positions = getPositions(df[, c("lat", "lon")])
 
 map = leaflet(df) %>% addTiles() %>%
-  addMarkers(~lon, ~lat, icon = icons)
+  addProviderTiles(providers$CartoDB.Positron)
+
+# adding markers to the map
+for(i in 1:nrow(df))
+{
+  row = as.data.frame(df[i,])
+  
+  if(row$positions == 'center')
+  {
+    map = map %>% addMarkers(map, lng = row$lon, lat = row$lat,
+               label = row$lat,
+               labelOptions = labelOptions(
+                 textOnly = TRUE,
+                 noHide = TRUE,
+                 style = list("font-weight" = "700"),
+                 sticky = "bottom",
+                 direction = "center",
+               ),
+               icon = icons(
+                 iconUrl = ifelse(row$x == 1, 'markers/red_marker.png',
+                                  ifelse(row$x == 2, 'markers/orange_marker.png',
+                                         ifelse(row$x == 3, 'markers/yellow_marker.png', 'markers/green_marker.png'))),
+                 iconWidth = 38, iconHeight = 38,
+                 iconAnchorX = 19, iconAnchorY = 38,
+               ))
+    
+  } else if(row$positions == 'left')
+  {
+    map = map %>% addMarkers(map, lng = row$lon, lat = row$lat,
+                             label = row$lat,
+                             labelOptions = labelOptions(
+                               textOnly = TRUE,
+                               noHide = TRUE,
+                               style = list("font-weight" = "700"),
+                               sticky = "bottom",
+                               direction = "left",
+                             ),
+                             icon = icons(
+                               iconUrl = ifelse(row$x == 1, 'markers/red_marker.png',
+                                                ifelse(row$x == 2, 'markers/orange_marker.png',
+                                                       ifelse(row$x == 3, 'markers/yellow_marker.png', 'markers/green_marker.png'))),
+                               iconWidth = 38, iconHeight = 38,
+                               iconAnchorX = 19, iconAnchorY = 38,
+                             ))
+  } else
+  {
+    map = map %>% addMarkers(map, lng = row$lon, lat = row$lat,
+                             label = row$lat,
+                             labelOptions = labelOptions(
+                               textOnly = TRUE,
+                               noHide = TRUE,
+                               style = list("font-weight" = "700"),
+                               sticky = "bottom",
+                               direction = "right",
+                             ),
+                             icon = icons(
+                               iconUrl = ifelse(row$x == 1, 'markers/red_marker.png',
+                                                ifelse(row$x == 2, 'markers/orange_marker.png',
+                                                       ifelse(row$x == 3, 'markers/yellow_marker.png', 'markers/green_marker.png'))),
+                               iconWidth = 38, iconHeight = 38,
+                               iconAnchorX = 19, iconAnchorY = 38,
+                             ))
+  }
+}
+
+
+
+
 map
 
 # radar chart
 #df_radar = df[c(1:4)] %>% slice(1)
-df_radar = df[c(1:4)]
+df_radar = df[c(2:5)]
 df_radar = rbind(rep(7, 4), rep(0, 4), df_radar)
 
 colors_border=c( rgb(0.2,0.5,0.5,0.6), rgb(0.8,0.2,0.5,0.6) , rgb(0.7,0.5,0.1,0.6), rgb(0.3,0.7,0.1,0.6) )
